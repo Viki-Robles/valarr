354f94d7c702152974e130c074138ed968188de4
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-08-17 17:24:46 +0100

    fixing the style of the main page

--- src/SectionOne/SectionOne.js
+++ src/SectionOne/SectionOne.js
@@ -5,10 +5,14 @@ export default function SectionOne() {
     return (
         <div className="sectionOne">
             <div className="sectionOne-Items">
-                <div className="sectionOne-itemA">holla</div>
+                <div className="sectionOne-itemA">
+                    <h2 className="logo">VΑL</h2>
+                    <h2 className="logo">ΛRR</h2>
+                </div>
                 <div className="sectionOne-itemB">
-                    <h2>Bringing Brands to Life Digitally.</h2>
-                    <button>VIEW OUR WORK</button>
+                    <h2 className="sectionOne-itemB-header">Building Digital</h2>
+                    <h2 className="sectionOne-itemB-header">Time Wraps.</h2>
+                    <button className="sectionOne-button">VIEW OUR WORK</button>
                 </div>
             </div>
         </div>
